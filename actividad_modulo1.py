728931eae58661bba186f9199d95cf12ad1b95c5
pasche21 <TOMASPASC42@GMAIL.COM>
2025-08-06 12:28:22 +0000

    actividad 1 modulos

---
+++ actividad_modulo1.py
@@ -0,0 +1,18 @@
+import random
+
+num1 = random.randint(0, 20)
+num2 = random.randint(0, 20)
+num3 = random.randint(0, 20)
+num4 = random.randint(0, 20)
+num5 = random.randint(0, 20)
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+total = num1 + num2 + num3 + num4 + num5
+
+print("La suma de los números generados es: " + str(total))
+
