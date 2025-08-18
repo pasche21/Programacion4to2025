20619433fc90cecbe7e24e9080935a6a12f51595
pasche21 <TOMASPASC42@GMAIL.COM>
2025-08-18 17:17:31 +0000

    py listas2 act2

---
+++ Act_bucles_for1.py
@@ -0,0 +1,19 @@
+# Solicitar una palabra al usuario
+palabra = input("Ingrese una palabra: ")
+
+# Mostrar mensaje informativo
+print("Las letras de la palabra son:")
+
+# Recorrer y mostrar cada carácter de la palabra
+for letra in palabra:
+    print(letra)
+
+# Solicitar una palabra al usuario
+palabra = input("Ingrese una palabra: ")
+
+# Mostrar mensaje informativo
+print("Las letras de la palabra son:")
+
+# Recorrer y mostrar cada carácter de la palabra
+for letra in palabra:
+    print(letra)
